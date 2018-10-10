8657c4c99c5bb8a817703c769f86b4e129a2ecdc
RIYA <rsriya383@gmail.com>
2018-10-10 20:05:40 +0530

    first draft of main code

---
+++ test_cases.py
@@ -0,0 +1,30 @@
+# boolean determining whether satellite in SSO or PO
+# 1 means SSO
+# 0 means PO
+orbitbool = 1
+
+# boolean determining whether disturbance will act or not
+# 1 means disturbance present
+# 0 means disturbance absent
+distbool = 1
+
+# boolean determining whether sensor modelling will be used or not
+# 1 means sensor-modelling present
+# 0 means sensor modelling absent
+sensbool = 1
+
+# boolean determining whether estimator will be used or not
+# 1 means estimator used
+# 0 means estimator not used
+estbool = 1
+
+# constant determining which controller will act or not
+# 1 means detumbing_controller present
+# 0 means controller absent
+contcons = 0
+
+
+# boolean determining whether actuator modelling will be used or not
+# 1 means actuator-modelling present
+# 0 means actuator modelling absent
+actbool = 1
