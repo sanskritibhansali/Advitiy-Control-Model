bb1129c06ebff8b8c413b7ea4eb12a493a27ddc4
RIYA <rsriya383@gmail.com>
2018-10-10 15:17:18 +0530

    Add files via upload

--- default_blocks.py
+++ default_blocks.py
@@ -3,26 +3,34 @@ import numpy as np
 import frames as fs
 #Default models of sensor : (ideal sensor) measured output = actual output (no noise or bias)
 
-def sunsensor(sat,v_sv_i):
-	v_sv_b = qnv.quatRotate(sat.getQ_BI(),v_sv_i)
-	v_sv_o = qnv.quatRotate(sat.getQ_BO(),v_sv_i)
-	return v_sv_b, v_sv_o;
+def sunsensor(sat):
+	v_sv_o = sat.getSun_o()
+	v_q_BO = sat.getQ_BO()
+	v_sv_b = qnv.quatRotate(v_q_BO,v_sv_o)
+	return v_sv_b
 
-def magmeter(sat,v_mag_i):
-	v_mag_b = qnv.quatRotate(sat.getQ_BI(),v_mag_i)
-	v_mag_o = qnv.quatRotate(sat.getQ_BO(),v_mag_i)
-	return v_mag_b, v_mag_o;
+def magmeter(sat):
+	v_B_o = sat.getMag_o()
+	v_q_BO = sat.getQ_BO()
+	v_mag_b = qnv.quatRotate(v_q_BO,v_B_o)
+	return v_mag_b
 
-def gps():
-	m_sgp_output_i = np.genfromtxt('sgp_output.csv', delimiter=",")
-	np.savetxt('gps_output.csv',m_sgp_output_i, delimiter=",")
+def gps(sat):
+	v_pos_m = sat.getPos() 
+	v_vel_m = sat.getVel()
+	time_m = sat.getTime()
+
+	return np.hstack([v_pos_m,v_vel_m,time_m])
 
 def gyroscope(v_wBIB):
 	return v_wBIB
 
-def J2_propagator():
-	m_sgp_output_i = np.genfromtxt('sgp_output.csv', delimiter=",")
-	np.savetxt('J2_output.csv',m_sgp_output_i, delimiter=",")
+def J2_propagator(sat):
+	v_pos_m = sat.getPos() 
+	v_vel_m = sat.getVel()
+	time_m = sat.getTime()
+	
+	return np.hstack([v_pos_m,v_vel_m,time_m])
 
 #Default models of controller: (no controller)
 
--- test_default_blocks.py
+++ test_default_blocks.py
@@ -4,66 +4,74 @@ import satellite
 import frames 
 import default_blocks as defblock
 from ddt import ddt, data, unpack, file_data
+import qnv
 
 @ddt
 class TestDefaultBlocks(unittest.TestCase):
-
+	
 	@file_data("test-data/test_defaultsunsensor.json")
 	@unpack
 	def test_sunsensor(self,value): 
 		qBI = np.asarray(value[0])
-		v_sv_i = np.asarray(value[1])
-
+		v_sv_i=np.asarray(value[1])
 		v_pos_i = np.array([1e6,-2.03,-3.0])
 		v_vel_i = np.array([2.0e3,2.8,-73.2])
 		qBO = frames.qBI2qBO(qBI,v_pos_i,v_vel_i)
 		state = np.hstack((qBO,np.zeros(3)))
 		mySat = satellite.Satellite(state,12.0)
 		mySat.setQ_BI(qBI)
-		result = defblock.sunsensor(mySat,v_sv_i)
-		v_expected = value[2],value[3];
+		mySat.setSun_i(v_sv_i)
+		mySat.setPos(v_pos_i)
+		mySat.setVel(v_vel_i)
+		qOI = qnv.quatMultiplyNorm(qnv.quatInv(qBO),qBI)
+		result = defblock.sunsensor(mySat)
+		v_expected = value[2];
+
 		self.assertTrue(np.allclose(result,v_expected))
 	
 	@file_data("test-data/test_defaultsunsensor.json")
 	@unpack
 	def test_magmeter(self,value): 
 		qBI = np.asarray(value[0])
-		v_sv_i = np.asarray(value[1])
-
+		v_B_i=np.asarray(value[1])
 		v_pos_i = np.array([1e6,-2.03,-3.0])
 		v_vel_i = np.array([2.0e3,2.8,-73.2])
 		qBO = frames.qBI2qBO(qBI,v_pos_i,v_vel_i)
 		state = np.hstack((qBO,np.zeros(3)))
 		mySat = satellite.Satellite(state,12.0)
 		mySat.setQ_BI(qBI)
-		result = defblock.magmeter(mySat,v_sv_i)
-		v_expected = value[2],value[3];
+		mySat.setMag_i(v_B_i)
+		mySat.setPos(v_pos_i)
+		mySat.setVel(v_vel_i)
+		qOI = qnv.quatMultiplyNorm(qnv.quatInv(qBO),qBI)
+		result = defblock.magmeter(mySat)
+		v_expected = value[2];
+
 		self.assertTrue(np.allclose(result,v_expected))
 	
 	def test_gyroscope(self):
 		v_w_BI_b = np.array((-3.9999, 4.8575, 0))
 		self.assertTrue(np.allclose(defblock.gyroscope(v_w_BI_b),v_w_BI_b))
 
-	sgp_output = np.genfromtxt('sgp_output.csv',delimiter=",")	
-	defblock.J2_propagator()
-	defblock.gps()
-	gps_output= np.genfromtxt('gps_output.csv',delimiter=",")
-	J2_output = np.genfromtxt('J2_output.csv',delimiter=",")
-	T = sgp_output[:,0]
-	l = np.linspace(0,len(T),12,int)	#Sample 7 data points from entire file
-	
-	@data(l[0],l[1],l[2],l[3],l[4],l[5],l[6],l[7],l[8],l[9])
-	def test_GPS(self,value):
-		v_expected = self.sgp_output[int(value),1:4]
-		v_result = self.gps_output[int(value),1:4]
-		self.assertTrue(np.allclose(v_expected,v_result))
-	
-	@data(l[0],l[1],l[2],l[3],l[4],l[5],l[6],l[7],l[8],l[9])
-	def test_J2_propagator(self,value):
-		v_expected = self.sgp_output[int(value),1:4]
-		v_result = self.J2_output[int(value),1:4]
-		self.assertTrue(np.allclose(v_expected,v_result))
-		
+	def test_GPS(self):
+		qBO = np.array([.0,0.,0.,1.])
+		state = np.hstack((qBO,np.array([-0.1,0.39159385,-0.7])))
+		sat = satellite.Satellite(state,12.05)
+		sat.setPos(np.array([7070e3,0.,0.])) 
+		sat.setVel(np.array([5.60,-5.0,0.0]))
+		v_expected = np.array([7070e3,0.,0.,5.60,-5.0,0.0,12.05])
+		self.assertTrue(np.allclose(defblock.gps(sat),v_expected))
+
+
+	def test_J2_propagator(self):
+		qBO = np.array([.0,0.,0.,1.])
+		state = np.hstack((qBO,np.array([-0.1,0.39159385,-0.7])))
+		sat = satellite.Satellite(state,12.05)
+		sat.setPos(np.array([7070e3,0.,0.])) 
+		sat.setVel(np.array([5.60,-5.0,0.0]))
+		v_expected = np.array([7070e3,0.,0.,5.60,-5.0,0.0,12.05])
+		self.assertTrue(np.allclose(defblock.J2_propagator(sat),v_expected))
+
 	def test_controller(self):
 		qBO = np.array([.0,0.,0.,1.])
 		state = np.hstack((qBO,np.array([0.10050588,-0.05026119,-0.3014887])))
